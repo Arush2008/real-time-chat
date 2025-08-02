9fe626ea290e2409c73adb1ae3de10c1b8f1537e
HeroicP <23779@burnside.school.nz>
2025-08-02 22:52:32 +1200

    Switch to Render: Reset to full chat server

--- nordServer/index.js
+++ nordServer/index.js
@@ -8,7 +8,7 @@ const io = require('socket.io')(http, {
     }
 });
 
-// IMPORTANT: use the Railway port!
+// IMPORTANT: use the Render port!
 const PORT = process.env.PORT || 3000;
 
 // Basic route for health check
